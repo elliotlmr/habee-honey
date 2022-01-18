d9c4fd7794fb7d00268b26c66d3ca2fa93425080
Elliot Lemaire <lemaireelliot@gmail.com>
2022-01-18 12:01:23 +0100

    header fixed, footer created

--- components/globals/Layout/Footer.js
+++ components/globals/Layout/Footer.js
@@ -1,3 +1,34 @@
+import s from "../../../styles/components/globals/Layout/Footer.module.scss";
+import Link from "next/link";
+
 export default function Footer() {
-  return <h1>footer</h1>;
+  return (
+    <footer className={s.footer}>
+      <ul className={s.mainMenu}>
+        <li>
+          <Link href="/">- HOMEPAGE</Link>
+        </li>
+        <li>
+          <Link href="/journey">- JOURNEY</Link>
+        </li>
+        <li>
+          <Link href="/shop">- SHOP</Link>
+        </li>
+        <li>
+          <Link href="/contact">- CONTACT</Link>
+        </li>
+      </ul>
+      <ul className={s.subMenu}>
+        <li>
+          <Link href="/disclaimer">- DISCLAIMER</Link>
+        </li>
+        <li>
+          <Link href="https://shiro-e-portfolio.vercel.app/" passHref>
+            <a target="_blank">- CREATOR</a>
+          </Link>
+        </li>
+      </ul>
+      <p className={s.copyright}>©2022 Shiro-e "Elliot Lemaire"</p>
+    </footer>
+  );
 }
--- components/globals/Layout/Header.js
+++ components/globals/Layout/Header.js
@@ -1,7 +1,14 @@
 import s from "../../../styles/components/globals/Layout/Header.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Header() {
+  const router = useRouter();
+  useEffect(() => {
+    console.log(router);
+  }, []);
+
   return (
     <header className={s.container}>
       <Link href="/" passHref>
@@ -9,13 +16,37 @@ export default function Header() {
       </Link>
       <nav className={s.navigation}>
         <Link href="/journey" passHref>
-          <a className={s.navlink}>Journey</a>
+          <a
+            className={
+              router.pathname === "/journey"
+                ? `${s.navlink} ${s.active}`
+                : s.navlink
+            }
+          >
+            Journey
+          </a>
         </Link>
         <Link href="/shop" passHref>
-          <a className={s.navlink}>Shop</a>
+          <a
+            className={
+              router.pathname === "/shop"
+                ? `${s.navlink} ${s.active}`
+                : s.navlink
+            }
+          >
+            Shop
+          </a>
         </Link>
         <Link href="/contact" passHref>
-          <a className={s.navlink}>Contact</a>
+          <a
+            className={
+              router.pathname === "/contact"
+                ? `${s.navlink} ${s.active}`
+                : s.navlink
+            }
+          >
+            Contact
+          </a>
         </Link>
       </nav>
     </header>
